a58714f8294945d59910f153b545a50b8936a442
Pravin <pbhatuse@gmail.com>
2026-05-15 11:18:23 +0000

    PRODUCTION

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -21,8 +21,8 @@ module "alb" {
 module "compute" {
   source             = "../../modules/compute"
   environment        = "production"
-  ecr_repo_url       = "437229446821.dkr.ecr.ap-south-1.amazonaws.com/my-app" # Replace with your ECR
-  execution_role_arn  = module.security.execution_role_arn
+  ecr_repo_url       = data.terraform_remote_state.global_ecr.outputs.ecr_repository_url
+  execution_role_arn = module.security.execution_role_arn
   ecs_sg_id          = module.security.ecs_sg_id
   private_subnet_ids = module.networking.private_subnet_ids
   target_group_arn   = module.alb.target_group_arn
